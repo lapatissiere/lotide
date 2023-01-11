09d923064ba66dd9c9316788b38d31b695242161
Nazia Farooq <lapatissiere@yahoo.com>
2023-01-10 20:05:11 -0500

    created new assert command in this file

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👌  Assertion Passed: ", `${actual} === ${expected}`);
+  } else {
+    console.log("☠️  Assertion Failed: ", actual, "!==", expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
