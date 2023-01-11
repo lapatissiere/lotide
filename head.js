e37f2392637bcf384849333ea8fe39b0e33d912e
Nazia Farooq <lapatissiere@yahoo.com>
2023-01-10 20:50:05 -0500

    added head

--- head.js
+++ head.js
@@ -6,5 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
+let array = [1, 2, 3, 4, 5];
+function head(array) {
+  return array[0];
+}
+console.log(head(array));
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
