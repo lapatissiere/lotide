7683c6ec5a8cd07802d453bf85c458df229cc9ec
Nazia Farooq <lapatissiere@yahoo.com>
2023-01-18 01:38:21 -0500

    refactored tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👌  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`☠️  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 let array = [1, 2, 3, 4, 5];
 function tail(array) {
