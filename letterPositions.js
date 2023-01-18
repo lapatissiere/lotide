78c8518c8f882d214b2ba5c4950fa3a0923ec5d4
Nazia Farooq <lapatissiere@yahoo.com>
2023-01-17 23:01:06 -0500

    letterPositions file added

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArray(actual, expected)) {
+    console.log(`👌  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☠️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let eqArray = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("hello"));
+
+//in each loop: check if results[sentence[index]] exists
+//1) exist; add the index to the array
+//2) doesnot exist: create an array holding the index (e.g [index])
