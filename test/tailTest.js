aeff566378f2e888ff8dd44db3850dc3cbdb0234
Nazia Farooq <lapatissiere@yahoo.com>
2023-01-18 16:34:45 -0500

    refactored tailTest.js per Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-console.log(tail([4, 5, 6, 7, 8, 9]));
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [5, 6, 7, 8, 9] for [4, 5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(tail([4, 5, 6, 7, 8, 9]), [5, 6, 7, 8, 9]);
+  });
+});
